d0cf2875ae7403ba7ab68bd1b71cf640667d5d5c
Nikita Shmorin <nikita.shmorin@yandex.ru>
2026-02-11 11:08:35 +0300

    add storage

--- internal/controller/postgres/statefulset.go
+++ internal/controller/postgres/statefulset.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
 
@@ -40,6 +41,31 @@ func BuildStatefulSet(
 							Image: "postgres:15",
 							Ports: []corev1.ContainerPort{
 								{ContainerPort: 5432},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "data",
+									MountPath: "/var/lib/postgresql/data",
+								},
+							},
+						},
+					},
+				},
+			},
+			VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "data",
+					},
+					Spec: corev1.PersistentVolumeClaimSpec{
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteOnce,
+						},
+						Resources: corev1.VolumeResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceStorage: resource.MustParse(
+									cluster.Spec.Storage.Size,
+								),
 							},
 						},
 					},
